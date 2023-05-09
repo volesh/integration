34d2ef080670c104eecd304d2e629d6169159c40
volesh <volesh2@gmail.com>
2023-05-09 15:38:21 +0300

    add commit

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,12 @@ app.get("/", (req, res) => {
 });
 
 app.post("/webhook", async (req: Request, res: Response) => {
+  console.log("code in endpoint", code);
   const data = await fetch(
     `https://api.timelyapp.com/1.1/oauth/token?redirect_uri=https://misty-erin-armadillo.cyclic.app/getToknes&code=${code}&client_id=bZS8omITNNblYpEA-AI1bVTWQ_pB_rgE8n_kyySrz7A&client_secret=T-9KGYgTiZqmVaYoqSckUPHPnvhiFy7-IG0tv-lpdq4&grant_type=authorization_code`,
     { method: "POST" }
   );
+
   console.log(data);
   res.end();
 });
