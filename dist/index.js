5cfe80138c35067eeb720ccf7d7ec434df3bee52
volesh <volesh2@gmail.com>
2023-05-10 11:28:11 +0300

    find account and user

--- dist/index.js
+++ dist/index.js
@@ -24,6 +24,19 @@ app.get("/", (req, res) => {
 });
 app.post("/webhook", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const project = req.body.project;
+    // Get email adres
+    const user = yield fetch(`https://vladscompany3.teamwork.com/people/${req.body.eventCreator.id}.json`, {
+        headers: {
+            Authorization: "Basic " + "twp_B8MG9eAALkD2fS8QTPHh3djd1O8T" + ":a",
+        },
+    }).then((data) => data.json());
+    const account = yield fetch("https://api.timelyapp.com/1.1/accounts", {
+        headers: {
+            Authorization: "Bearer " + "VgGvnfBPk-c7oeohnQz6JEAp1AveEeyxpAwdsDNqw6I",
+        },
+    }).then((data) => data.json())[0];
+    console.log("User", user);
+    console.log("Account", account);
     // const data = await fetch(
     //   `https://api.timelyapp.com/1.1/oauth/token?redirect_uri=https://careful-wig-cow.cyclic.app/getToknes&code=${code}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=authorization_code`,
     //   { method: "POST" }
--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,22 @@ app.get("/", (req, res) => {
 
 app.post("/webhook", async (req: Request, res: Response) => {
   const project = req.body.project;
+
+  // Get email adres
+  const user = await fetch(`https://vladscompany3.teamwork.com/people/${req.body.eventCreator.id}.json`, {
+    headers: {
+      Authorization: "Basic " + "twp_B8MG9eAALkD2fS8QTPHh3djd1O8T" + ":a",
+    },
+  }).then((data) => data.json());
+
+  const account = await fetch("https://api.timelyapp.com/1.1/accounts", {
+    headers: {
+      Authorization: "Bearer " + "VgGvnfBPk-c7oeohnQz6JEAp1AveEeyxpAwdsDNqw6I",
+    },
+  }).then((data) => data.json())[0];
+  console.log("User", user);
+  console.log("Account", account);
+
   // const data = await fetch(
   //   `https://api.timelyapp.com/1.1/oauth/token?redirect_uri=https://careful-wig-cow.cyclic.app/getToknes&code=${code}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=authorization_code`,
   //   { method: "POST" }
